651dae93317d51faef2ef27608574726d3c7bea1
Zabrimus <github@hannebauer.org>
2025-04-29 13:09:28 +0200

    update streams

--- tool/get_all_kodinerds_streams.sh
+++ tool/get_all_kodinerds_streams.sh
@@ -3,6 +3,16 @@
 # clone the repository
 git clone --depth 1 https://github.com/jnk22/kodinerds-iptv
 
+# apply pull requests
+cd kodinerds-iptv
+for i in $(ls ../pull_requests); do
+   patch -p1 < ../pull_requests/$i
+done
+
+scripts/yaml_to_m3u.py
+
+cd ..
+
 # rm unwanted
 rm kodi_radio.m3u
 
